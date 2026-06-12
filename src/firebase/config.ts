bb1dac58cecaa6ad039e10579618c029532ef126
Nomis 108a <nomis108a@gmail.com>
2026-06-12 12:47:43 +0000

    I'm getting "Firebase: Error (auth/api-key-not-valid.-please-pass-a-vali

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
  * Values are populated from environment variables.
  */
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDummyKey",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB_REPLACE_WITH_ACTUAL_KEY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "primefunded-8b8a5.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "primefunded-8b8a5",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "primefunded-8b8a5.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "777000000000",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:777000000000:web:abcdef123456",
 };
