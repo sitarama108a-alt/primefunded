94220a39aefa4a76478eedeec1fbf2e212a11b91
the prime funded <sitarama108a@gmail.com>
2026-06-18 08:11:47 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://primefunded.com';
+  const baseUrl = 'https://primefunded.fund';
   
   return [
     { url: baseUrl, lastModified: new Date(), changeFrequency: 'daily', priority: 1 },
