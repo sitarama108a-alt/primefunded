ab3182e8636e942b8b132c08f78bcce5f52706e1
Nomis 108a <nomis108a@gmail.com>
2026-06-28 16:16:28 +0000

    src/app/demo/ChartSettingsModal.tsx throws "DialogFooter is not defined"

--- src/app/demo/ChartSettingsModal.tsx
+++ src/app/demo/ChartSettingsModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
